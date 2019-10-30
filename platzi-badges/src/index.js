f8a405cee560ece6f6d7d65cb5cd5524b403ff6c
Lisandro Gomez <draivenl@gmail.com>
2019-10-30 10:21:23 -0500

    Entendiendo las diferencias entre utilizar JSX y React.createElement

--- platzi-badges/src/index.js
+++ platzi-badges/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-const element = <h1><i>Hola</i> Mundo</h1>
+const name = 'Lisandro'
+
+const elementJSX = <h1><i>Hola</i> {name}</h1>
+// Alternativa para agegar el elemento no con JSX sino con React
+
+const element = React.createElement('a', {href: 'http://platzi.com', target: '_blank'}, `Hola ${name}, clic para ir a Platzi`)
 
 const container = document.getElementById('app')
 
 ReactDom.render(element, container)
+// ReactDom.render(elementJSX, container)
