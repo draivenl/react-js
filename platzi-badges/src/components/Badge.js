d2493e9770b189a05109f1af00d3c6ef705ccc52
Lisandro Gomez <draivenl@gmail.com>
2019-10-30 12:24:47 -0500

    Aplicando estilos al componente

--- platzi-badges/src/components/Badge.js
+++ platzi-badges/src/components/Badge.js
@@ -1,22 +1,23 @@
 import React from "react";
 import confLogo from '../images/badge-header.svg'
+import  "./styles/Badge.css";
 
 class Badge extends React.Component {
     render() {
         return (
-            <div>
-                <div>
+            <div className='Badge'>
+                <div className='Badge__header'>
                     <img src={confLogo} alt="Logo de la conferencia"></img>
                 </div>
-                <div>
-                    <img src='https://s.gravatar.com/avatar/acc8c5d53c0eddd70332fa11c828665f?s=80' alt='Avatar'></img>
+                <div className='Badge__section-name'>
+                    <img className='Badge__avatar' src='https://s.gravatar.com/avatar/acc8c5d53c0eddd70332fa11c828665f?s=80' alt='Avatar'></img>
                     <h1>Lisandro <br/>G&oacute;mez Ortiz</h1>
                 </div>
-                <div>
+                <div className='Badge__section-info'>
                     <p>FullStack Engineer</p>
                     <p>@lgomezo</p>
                 </div>
-                <div>
+                <div className='Badge__footer'>
                     #PlatziConf
                 </div>
             </div>
