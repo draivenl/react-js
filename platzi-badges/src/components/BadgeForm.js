ca70c4745f96fa5b99ff96fab371af4ba268d27a
Lisandro Gomez <draivenl@gmail.com>
2019-10-31 01:45:45 -0500

    Manejo de estado con this.setState y this.state, inputs controlados para evitar guardar doblemente en memoria

--- platzi-badges/src/components/BadgeForm.js
+++ platzi-badges/src/components/BadgeForm.js
@@ -1,16 +1,27 @@
 import React from "react"
 
 class BadgeForm extends React.Component{
+    state = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        jobTitle: '',
+        twitter: ''
+    }
+
     handleChange = ev =>{
-        console.log({
-            id: ev.target.id,
-            name: ev.target.name,
-            value: ev.target.value
-        });
+        // console.log({
+        //     id: ev.target.id,
+        //     name: ev.target.name,
+        //     value: ev.target.value
+        // });
+        this.setState({
+            [ev.target.name]: ev.target.value
+        })
         
     }
 
-    handleClick = ev => console.log("Button Clicked")
+    handleClick = ev => console.log(this.state)
     
     // Los button por defecto son type="submit"
     // Si se quiere evitar que haga submit por defecto, se agrega el evento onSubmit
@@ -20,7 +31,6 @@ class BadgeForm extends React.Component{
     handleSubmit = ev => {
         ev.preventDefault()
     }
-    
 
     render(){
         return(
@@ -30,7 +40,23 @@ class BadgeForm extends React.Component{
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
                         <label>First Name</label>
-                        <input onChange={this.handleChange} className="form-control" type="text" name="firstName" id="firstName"/>
+                        <input onChange={this.handleChange} className="form-control" type="text" name="firstName" id="firstName" value={this.state.firstName}/>
+                    </div>
+                    <div className="form-group">
+                        <label>Last Name</label>
+                        <input onChange={this.handleChange} className="form-control" type="text" name="lastName" id="lastName" value={this.state.lasName}/>
+                    </div>
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input onChange={this.handleChange} className="form-control" type="email" name="email" id="email" value={this.state.email}/>
+                    </div>
+                    <div className="form-group">
+                        <label>Job Title</label>
+                        <input onChange={this.handleChange} className="form-control" type="text" name="jobTitle" id="jobTitle" value={this.state.jobTitle}/>
+                    </div>
+                    <div className="form-group">
+                        <label>Twitter</label>
+                        <input onChange={this.handleChange} className="form-control" type="text" name="twitter" id="twitter" value={this.state.twitter}/>
                     </div>
                     <button type="button" onClick={this.handleClick} className="btn btn-primary">Save</button>
                 </form>
