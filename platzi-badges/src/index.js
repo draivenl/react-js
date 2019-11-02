2e50c747bc3b9aab1e8a89c7d2477c66fcc7fb37
Lisandro Gomez <draivenl@gmail.com>
2019-11-02 18:32:36 -0500

    Agregando listas

--- platzi-badges/src/index.js
+++ platzi-badges/src/index.js
@@ -3,11 +3,11 @@ import ReactDom from 'react-dom'
 import "bootstrap/dist/css/bootstrap.css"
 import './global.css'
 
-import BadgeNew from "./pages/BadgeNew";
+import Badges from "./pages/Badges";
 
 const container = document.getElementById('app')
 
 ReactDom.render(
-    <BadgeNew />, 
+    <Badges />, 
     container
 )
