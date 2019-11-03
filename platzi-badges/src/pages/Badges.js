3e64f2a676a5abb4590b817d25fa4cb0ff9d668d
Lisandro Gomez <draivenl@gmail.com>
2019-11-03 01:15:28 -0500

    Practicando con el ciclo de vida de los componentes

--- platzi-badges/src/pages/Badges.js
+++ platzi-badges/src/pages/Badges.js
@@ -5,41 +5,80 @@ import BadgesList from "../components/BadgesList";
 import {Link} from "react-router-dom"
 
 class Badges extends React.Component{
-    state = {
-        data: [
-            {
-              id: '2de30c42-9deb-40fc-a41f-05e62b5939a7',
-              firstName: 'Freda',
-              lastName: 'Grady',
-              email: 'Leann_Berge@gmail.com',
-              jobTitle: 'Legacy Brand Director',
-              twitter: 'FredaGrady22221-7573',
-              avatarUrl:
-                'https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon',
-            },
-            {
-              id: 'd00d3614-101a-44ca-b6c2-0be075aeed3d',
-              firstName: 'Major',
-              lastName: 'Rodriguez',
-              email: 'Ilene66@hotmail.com',
-              jobTitle: 'Human Research Architect',
-              twitter: 'MajorRodriguez61545',
-              avatarUrl:
-                'https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon',
-            },
-            {
-              id: '63c03386-33a2-4512-9ac1-354ad7bec5e9',
-              firstName: 'Daphney',
-              lastName: 'Torphy',
-              email: 'Ron61@hotmail.com',
-              jobTitle: 'National Markets Officer',
-              twitter: 'DaphneyTorphy96105',
-              avatarUrl:
-                'https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon',
-            },
-          ]
+    constructor(props){
+        super(props)
+        console.log('1. Constructor');
+
+        this.state = {
+            data: []
+        }
     }
+    componentDidMount(){
+        console.log('3. ComponentDidMount');
+        // El setTimeOut devuelve un ID, este se puede utilizar en el componentWillUnmount
+        // para validar que no salga error si se va a desmontar el componene y aún no se
+        // tiene una respuesta
+        this.timeOutId = setTimeout(() => {
+            this.setState({
+                data:[
+                    {
+                      id: '2de30c42-9deb-40fc-a41f-05e62b5939a7',
+                      firstName: 'Freda',
+                      lastName: 'Grady',
+                      email: 'Leann_Berge@gmail.com',
+                      jobTitle: 'Legacy Brand Director',
+                      twitter: 'FredaGrady22221-7573',
+                      avatarUrl:
+                        'https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon',
+                    },
+                    {
+                      id: 'd00d3614-101a-44ca-b6c2-0be075aeed3d',
+                      firstName: 'Major',
+                      lastName: 'Rodriguez',
+                      email: 'Ilene66@hotmail.com',
+                      jobTitle: 'Human Research Architect',
+                      twitter: 'MajorRodriguez61545',
+                      avatarUrl:
+                        'https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon',
+                    },
+                    {
+                      id: '63c03386-33a2-4512-9ac1-354ad7bec5e9',
+                      firstName: 'Daphney',
+                      lastName: 'Torphy',
+                      email: 'Ron61@hotmail.com',
+                      jobTitle: 'National Markets Officer',
+                      twitter: 'DaphneyTorphy96105',
+                      avatarUrl:
+                        'https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon',
+                    },
+                  ]
+            })
+        }, 3000);
+    }
+
+    componentDidUpdate(previusProps, previusState){
+        console.log('5. componentDidUpdate');
+        console.log({
+            previusProps,
+            previusState
+        });
+
+        console.log({
+            props:this.props,
+            state: this.state
+        });
+    }
+
+    componentWillUnmount(){
+        console.log('6. componentWillUnmount');
+        // Limpia el setTimeout en caso de que no haya terminado cuando se va
+        // a desmontar
+        clearTimeout(this.timeOutId)
+        
+    }
+
     render(){
+        console.log('2/4. Render')
         return(
             <React.Fragment>
                 <div className="Badges">
